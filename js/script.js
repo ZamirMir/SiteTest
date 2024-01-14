29ce381271c8036d861cb92bc164689cd7bbff03
Zamir Mushtaq <zamirmir67@gmail.com>
2024-01-14 08:46:09 +0300

    cloning site first
    Frist commit in this!

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// <!--Start of Tawk.to Script-->
+
+
+    var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+    (function () {
+        var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+        s1.async = true;
+        s1.src = 'https://embed.tawk.to/640c426f4247f20fefe540d4/1gr7u71gs';
+        s1.charset = 'UTF-8';
+        s1.setAttribute('crossorigin', '*');
+        s0.parentNode.insertBefore(s1, s0);
+    })();
+
+
+
+//Blocking the inspect element
+
+// document.addEventListener('contextmenu', function (e) {
+//     e.preventDefault();
+// });
+
+
